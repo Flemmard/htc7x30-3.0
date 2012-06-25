098125fcd20222eb7d515471da7ac2cae4636ecd
Flemmard <flemmard@gmail.com>
2012-06-25 13:32:18 -0400

    Fixed totally headset on spade

--- arch/arm/mach-msm/board-spade-keypad.c
+++ arch/arm/mach-msm/board-spade-keypad.c
@@ -40,7 +40,7 @@ static void config_gpio_table(uint32_t *table, int len)
 	for (n = 0; n < len; n++) {
 		rc = gpio_tlmm_config(table[n], GPIO_CFG_ENABLE);
 		if (rc) {
-			pr_err("[CAM] %s: gpio_tlmm_config(%#x)=%d\n",
+			pr_err("[KEY] %s: gpio_tlmm_config(%#x)=%d\n",
 				__func__, table[n], rc);
 			break;
 		}
