3cedc0ebfe1adaa7e79334ca053e5d7c0323f978
Flemmard <flemmard@gmail.com>
2012-07-18 16:44:45 +0200

    bcmdhd: p2p separate interface can now be set via defconfig (CONFIG_BCMDHD_P2P_INTERFACE)

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -121,7 +121,12 @@ u32 wl_dbg_level = WL_DBG_ERR ;
 /* Set this to 1 to use a seperate interface (p2p0)
  *  for p2p operations.
  */
+
+#ifdef CONFIG_BCMDHD_P2P_INTERFACE
 #define ENABLE_P2P_INTERFACE	0
+#else
+#define ENABLE_P2P_INTERFACE	0
+#endif
 
 /* This is to override regulatory domains defined in cfg80211 module (reg.c)
  * By default world regulatory domain defined in reg.c puts the flags NL80211_RRF_PASSIVE_SCAN
