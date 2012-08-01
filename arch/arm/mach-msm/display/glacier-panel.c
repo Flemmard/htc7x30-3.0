2d9b56b49de20b17ea86efc6cb422ecafffc3fbf
Nick Reuter <nreuter85@gmail.com>
2012-08-01 18:47:23 -0500

    Glacier: Add HW_VSYNC support

--- arch/arm/mach-msm/display/glacier-panel.c
+++ arch/arm/mach-msm/display/glacier-panel.c
@@ -736,19 +736,33 @@ static struct platform_driver glacier_backlight_driver = {
 };
 
 static struct msm_mdp_platform_data mdp_pdata_sharp = {
-	.overrides = MSM_MDP_PANEL_FLIP_UD | MSM_MDP_PANEL_FLIP_LR
 #ifdef CONFIG_OVERLAY_FORCE_UPDATE
-	 | MSM_MDP_FORCE_UPDATE
+	.overrides = MSM_MDP_PANEL_FLIP_UD | MSM_MDP_PANEL_FLIP_LR | MSM_MDP_FORCE_UPDATE,
+#else
+	.overrides = MSM_MDP_PANEL_FLIP_UD | MSM_MDP_PANEL_FLIP_LR,
 #endif
-	,
+#ifdef CONFIG_MDP4_HW_VSYNC
+       .xres = 480,
+       .yres = 800,
+       .back_porch = 20,
+       .front_porch = 20,
+       .pulse_width = 40,
+#endif
 };
 
 static struct msm_mdp_platform_data mdp_pdata_common = {
-	.overrides = MSM_MDP4_MDDI_DMA_SWITCH
 #ifdef CONFIG_OVERLAY_FORCE_UPDATE
-	 | MSM_MDP_FORCE_UPDATE
+	.overrides = MSM_MDP4_MDDI_DMA_SWITCH | MSM_MDP_FORCE_UPDATE,
+#else
+	.overrides = MSM_MDP4_MDDI_DMA_SWITCH,
 #endif
-	,
+#ifdef CONFIG_MDP4_HW_VSYNC
+       .xres = 480,
+       .yres = 800,
+       .back_porch = 4,
+       .front_porch = 2,
+       .pulse_width = 4,
+#endif
 };
 
 int __init glacier_init_panel(void)
